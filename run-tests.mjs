8270fd849ec469c1a6b91d653a4c49e4eee14b02
reaganscofield <reaganscofieldkayembe@outlook.com>
2025-09-08 21:24:10 +0200

    Intials commit

--- run-tests.mjs
+++ run-tests.mjs
@@ -3,7 +3,7 @@ import { execSync } from 'child_process';
 try {
   console.log('Running Cucumber Tests Script');
   // Set environment variables for tsx
-  process.env.NODE_OPTIONS = '--loader tsx/esm';
+  process.env.NODE_OPTIONS = '--import tsx/esm';
   // Command to run the tests and point to the features and steps files
   const command = [
     'cucumber-js',
@@ -14,7 +14,7 @@ try {
   // Execute the command
   execSync(command, { 
     stdio: 'inherit',
-    env: { ...process.env, NODE_OPTIONS: '--loader tsx/esm' }
+    env: { ...process.env, NODE_OPTIONS: '--import tsx/esm' }
   });
 } catch (error) {
   console.error('Tests failed:', error.message);
