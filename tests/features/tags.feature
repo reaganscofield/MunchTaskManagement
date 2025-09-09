b0dac6da9ccd569f81162acbe756b12a07a0fbc9
reaganscofield <reaganscofieldkayembe@outlook.com>
2025-09-09 08:50:24 +0200

    Initials commit

--- tests/features/tags.feature
+++ tests/features/tags.feature
@@ -5,6 +5,7 @@ Feature: Tag Management
   So that I can categorize tasks
 
   Scenario: User can retrieve tags
+    Given I am authenticated
     When I get all tags
     Then I should get a success response with status code "200" and message "Successfully retrieved tags" with the tags data
     And I should see a list of tags
--- tests/steps/tags.steps.ts
+++ tests/steps/tags.steps.ts
@@ -6,9 +6,12 @@ let response;
 const { expect } = chai;
 chai.use(chaiHttp);
 const baseUrl: string = 'http://localhost:3005';
+declare global {
+  var authToken: string | null;
+}
 
 When('I get all tags', async function () {
-  response = await chai.request(baseUrl).get('/tags');
+  response = await chai.request(baseUrl).get('/tags').set('Authorization', `Bearer ${global.authToken}`);
 });
 
 Then('I should get a success response with status code {string} and message {string} with the tags data', function (statusCode: string, message: string) {
